408a030bd72ae1e3d334d5580119bdf7b42bfb74
Chris Barnes <barnesc@janelia.hhmi.org>
2020-11-16 23:37:36 +0000

    add setup.py

--- setup.py
+++ setup.py
@@ -6,27 +6,27 @@ from setuptools import setup, find_packages
 
 here = pathlib.Path(__file__).parent.resolve()
 
-long_description = (here / 'README.rst').read_text(encoding='utf-8')
+long_description = (here / "README.rst").read_text(encoding="utf-8")
 
 setup(
-    name='backports.strenum',  # Required
-    version='0.1.0',  # Required
-    description='Base class for creating enumerated constants that are also subclasses of str',  # Optional
-    long_description=long_description,  # Optional
-    long_description_content_type='text/x-rst',  # Optional (see note above)
-    url='https://github.com/clbarnes/backports.strenum',  # Optional
-    classifiers=[  # Optional
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3 :: Only',
+    name="backports.strenum",
+    version="0.1.0",
+    description="Base class for creating enumerated constants that are also subclasses of str",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/clbarnes/backports.strenum",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
     ],
-    keywords='backports, enum, strenum',  # Optional
-    packages=find_packages(exclude='tests'),  # Required
+    keywords="backports, enum, strenum",
+    packages=find_packages(exclude="tests"),
     test_suite="tests",
-    python_requires='>=3.6, <4',
+    python_requires=">=3.6, <4",
 )
