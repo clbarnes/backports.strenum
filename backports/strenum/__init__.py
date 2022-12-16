69a97d68fb8937187a82eae6fc01e8351670eba5
Thomas MK <tmke@posteo.net>
2022-12-16 13:33:39 +0100

    Use StrEnum from the standard library if available
    This allows people to just import from `backports.strenum`
    on Python 3.11 and still get the version from the standard
    library. Otherwise you need the if statement on every
    import.
    
    I used `if sys.version_info >= (3, 11)` instead of
    `try: from enum import StrEnum` because static type checkers know how to
    interpret the former, but struggle with the latter.

--- backports/strenum/__init__.py
+++ backports/strenum/__init__.py
@@ -1,4 +1,8 @@
-from .strenum import StrEnum
+import sys
 from .version import version as __version__  # noqa: F401
+if sys.version_info >= (3, 11):
+    from enum import StrEnum
+else:
+    from .strenum import StrEnum
 
 __all__ = ["StrEnum"]
