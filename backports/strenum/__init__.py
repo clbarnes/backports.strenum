a9615cf8733431091f4f08a4a8232d5fb910cd93
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2023-02-07 14:40:29 +0000

    Add __version_info__

--- backports/strenum/__init__.py
+++ backports/strenum/__init__.py
@@ -1,4 +1,5 @@
 from .strenum import StrEnum
 from .version import version as __version__  # noqa: F401
+from .version import version_tuple as __version_info__  # noqa: F401
 
 __all__ = ["StrEnum"]
