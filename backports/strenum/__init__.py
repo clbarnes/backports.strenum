4d7b4aa0b2b3622dfa75ccd748ebb389119edece
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2023-09-19 16:13:18 +0100

    Move from setuptools_scm to poetry

--- backports/strenum/__init__.py
+++
@@ -1,5 +0,0 @@
-from .strenum import StrEnum
-from .version import version as __version__  # noqa: F401
-from .version import version_tuple as __version_info__  # noqa: F401
-
-__all__ = ["StrEnum"]
